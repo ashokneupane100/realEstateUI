0ec8b422fc517c894b372399809cc6d4c2ba970b
ashokneupane100 <ashokabrother@gmail.com>
2024-10-19 21:52:39 +0545

    SearchBar is being made

---
+++ src/components/navbar/searchbar/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function SearchBar() {
+  return (
+    <div>
+      SearchBar
+      <div className="type"></div>
+      <form action="">
+        <input type="text" name="location" placeholder="City location" />
+      </form>
+    </div>
+  );
+}
--- src/routes/homePage.jsx
+++ src/routes/homePage.jsx
@@ -1,13 +1,50 @@
+// HomePage.jsx
 import React from 'react';
 import "./homePage.scss";
+import SearchBar from '../components/navbar/searchbar/SearchBar';
 
-function homePage() {
+const HomePage = () => {
+  const statistics = [
+    {
+      number: "16+",
+      label: "Years of Experience"
+    },
+    {
+      number: "200",
+      label: "Award Gained"
+    },
+    {
+      number: "2000",
+      label: "Property Ready"
+    }
+  ];
+
   return (
     <div className='homePage'>
-
-      
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className='title'>Find Real Estate & Get Your Dream Place</h1>
+          <p className="description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero neque hic 
+            mollitia quam, earum dolor repudiandae dolorum nesciunt, perspiciatis 
+            quisquam fugiat.
+          </p>
+          <SearchBar />
+          <div className="statistics">
+            {statistics.map((stat, index) => (
+              <div className="stat-box" key={index}>
+                <h2 className="stat-number">{stat.number}</h2>
+                <h3 className="stat-label">{stat.label}</h3>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="Real Estate Property" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default homePage
+export default HomePage;
